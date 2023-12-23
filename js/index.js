d70c2df0f158fbb3cc2d768ab59667238f5633aa
aMamdouh10 <amamdouhsaber22@gmail.com>
2023-12-23 21:19:19 +0200

    second commit

--- js/index.js
+++ js/index.js
@@ -1,36 +1,89 @@
 var bookName = document.getElementById("booktName");
 var bookModel = document.getElementById("bookModel");
 var bookList = [];
-// if(localStorage.getItem("bookList") != null){
-//     bookList =JSON.parse(localStorage.getItem("bookList"))
-//     displaybook()
-// }
+
+if(localStorage.getItem("books") != null)
+{
+    bookList = JSON.parse( localStorage.getItem("books"));
+    displayProudct(bookList)
+}
 
 function addBook(){
+if(validateBookName()&&validateBookUrl() == true){
     var book = {
         name:bookName.value,
         model:bookModel.value
     }
     bookList.push(book)
+        localStorage.setItem("books",JSON.stringify(bookList))
+
     displaybook(bookList)
-    // localStorage.setItem("bookList",JSON.stringify(bookList))
 }
 
-function displaybook(books) {
+}
+
+function displaybook() {
 var cartona =``;
-for (i =1;i<books.length;i++){
+for (i =1;i<bookList.length;i++){
     cartona+=`<tr>
     <td>${i}</td>
-    <td>${books[i].name}</td>
+    <td>${bookList[i].name}</td>
     <td>
         <button class="text-white btn btn-warning "><i class="fa-solid fa-eye"></i>
         Visite</button>
     </td>
     <td>
-        <button class="btn btn-danger "><i class="fa-solid fa-trash"></i>
+        <button onclick="deleteBook(${i})" class="btn btn-danger "><i class="fa-solid fa-trash"></i>
         Delete</button>
     </td>
 </tr>`
 }
 document.getElementById("tBody").innerHTML = cartona;
 }
+
+
+function deleteBook(bookIndex){
+    bookList.splice(bookIndex,1)
+    localStorage.setItem("books",JSON.stringify(bookList))
+    displaybook(bookList)
+}
+
+
+
+function validateBookName(){
+    var rejex = /^[a-zA-Z]{3,}$/;
+    if( rejex.test(bookName.value)){
+        bookName.style.border="none"
+        document.getElementById('wrongName').classList.add("d-none")
+
+        return true
+    }else{
+        bookName.style.border="5px red solid"
+        document.getElementById('wrongName').classList.remove("d-none")
+        return false
+    }
+}
+
+
+function validateBookUrl(){
+    var rejex = /^www\.[a-zA-Z]{3,20}\.com$/;
+    if( rejex.test(bookModel.value)){
+        bookModel.style.border="none"
+        document.getElementById('wrongUrl').classList.add("d-none")
+
+        return true
+    }else{
+        bookModel.style.border="5px red solid"
+        document.getElementById('wrongUrl').classList.remove("d-none")
+
+        return false
+    }
+}
+
+
+
+// function openModal(){
+//     var modal = document.getElementById("exampleModal")
+//     modal.classList.replace("d-none","d-bloc")
+    
+// }
